79e480bcf465b6d4a79362f5a3d0aedb602c2202
JS-011 <136203219+JS-011@users.noreply.github.com>
2026-04-14 15:18:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("DEBUG: Program is running")
+import sys
+from scheduler import schedule
+from io_utils import read_input, write_output
+
+def main():
+    input_file = sys.argv[1]
+
+    data = read_input(input_file)
+
+    policy = data["policy"]
+    jobs = data["jobs"]
+    quantum = data.get("quantum")
+
+    result = schedule(policy, jobs, quantum)
+
+    write_output(result)
+
+if __name__ == "__main__":
+    main()
