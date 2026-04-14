29fee109fc282ab9e775706fab74e733c3f70966
JS-011 <136203219+JS-011@users.noreply.github.com>
2026-04-14 15:17:51 -0400

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,8 @@
 from fifo import fifo
+from sjf import sjf
 
 def schedule(policy, jobs, quantum=None):
     if policy == "FIFO":
         return fifo(jobs)
+    elif policy == "SJF":
+        return sjf(jobs)
