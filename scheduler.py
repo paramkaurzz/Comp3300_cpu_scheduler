095ae12dda7ea2c9f26f28611076b1445a66ae46
y-n-g-king <musinguziemmanuelthe4@gmail.com>
2026-04-14 23:37:22 -0400

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,8 +1,13 @@
 from fifo import fifo
 from sjf import sjf
+from rr import rr
 
 def schedule(policy, jobs, quantum=None):
     if policy == "FIFO":
         return fifo(jobs)
     elif policy == "SJF":
         return sjf(jobs)
+    elif policy == "RR":
+        return rr(jobs, quantum)
+    else:
+        raise ValueError(f"Unsupported policy: {policy}")
